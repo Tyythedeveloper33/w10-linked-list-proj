2409d820ee0edd8a9daebf8814d2704061122204
tylerwebster <webstertyler81@yahoo.com>
2024-05-14 23:08:14 -0400

    finished queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -10,18 +10,54 @@ class Queue {
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode
 
-        // Your code here 
+        } else {
+            let curr = this.tail
+            while (!curr.next) {
+                curr.next = newNode
+                this.tail = newNode
+            }
+
+        }
+        this.length++
+        return this.length
+
+
+
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
+        const currVal = this.head
+        let res = []
+        if (this.length === 1) {
+            res.push(currVal)
+            this.head = null
+            this.tail = null
+            return res[0].value
+        }
+        if (!this.head) {
+            return null
+        }
 
-        // Your code here 
+
+        this.head = this.head.next
+       console.log(currVal.value)
+
+        this.length--
+
+        return currVal.value
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
+        // time complexity of o(1) space complexity of o(n)
     }
 
 }
